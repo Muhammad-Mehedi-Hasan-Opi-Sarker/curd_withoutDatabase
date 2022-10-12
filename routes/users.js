92101e70cea27f24cc1d0248515025851a28ca19
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2022-10-12 18:24:31 +0600

    post route add

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,20 @@ router.get("/", (req, res) => {
   res.send(users);
 });
 
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    const found = users.find((user) => user.id == id);
+    res.send(found);
+  });
+  
+  router.post("/", (req, res) => {
+    const user = req.body;
+    const Id = uuidv4();
+    const UserId = { ...user, id: Id };
+    users.push(UserId);
+    res.send(`Api is called ${user.firstName} added`);
+  });
+  
+  
 
 module.exports = router;
